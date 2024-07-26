e9bb2185763aa9a2c375099f297b34a6049bfaa4
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2024-07-26 10:51:46 +0300

    Add global auth state and context

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,9 +1,15 @@
+import { useContext } from "react";
 import { login } from "../api/authApi";
+import { AuthContext } from "../contexts/AuthContext";
 
 export const useLogin = () => {
+    const { changeAuthState } = useContext(AuthContext);
+
     const loginHandler = async (email, password) => {
         try {
             const result = await login(email, password);
+
+            changeAuthState(result);
         } catch (err) {
             console.error(err.message);
         }
